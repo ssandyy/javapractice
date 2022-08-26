619090405c493df0270704302738b83ad67b26f9
sandeep <ssandyy789@gmail.com>
2022-08-26 07:30:23 -0400

    ternary_operator

---
+++ array.java
@@ -0,0 +1,14 @@
+package learn_java;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {4,5,8,6,9,2};
+		
+		for (int b : arr) {
+			System.out.println(b);
+		}
+	}
+
+}
--- ternaryOperator.java
+++ ternaryOperator.java
@@ -5,7 +5,7 @@ public class ternaryOperator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=55, b=67, c=53;
+		int a=75, b=67, c=53;
 		//()?(): ()
 		//booleanExpression ? expression1 : expression2
 		System.out.println((a>b)?(a>c?a:c):(b>c?b:c));
